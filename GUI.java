889d7ff27ff8f3c53b3769c85860b34f47264415
LAURA CATALINA PRECIADO BALLEN <92498586+kt6delta@users.noreply.github.com>
2022-07-12 09:12:32 +0000

    .

--- GUI.java
+++ GUI.java
@@ -102,7 +102,32 @@ public class GUI extends JFrame implements ActionListener{
           date[f][c]=Integer.parseInt(txt[f][c].getText());
         }
       }
-      
+      Cola c = new Cola(this);
+      for(int n=0; n<5; n++){
+        c.Insert2(this.date[n][0],this.date[n][1],n);//id, rafaga,posicion
+      }
+    c.Calcula();
+    c.DeployCola();
+    System.out.println();
+    c.Change();
+
+    int l=1;
+    int l2=0;
+    //System.out.println("id cliente" +"  "+"transacciones");
+    for(int i=0; i<10; i++){
+      int num = (int)(Math.random()*4+2); //2-4
+      for(int n=1; n<num; n++){
+         c.Insert(l,(n+l2));
+         l++;
+       }
+        if(l2==0){
+          l2++;
+        }
+        c.Calcula();
+        c.DeployCola();
+        System.out.println();
+        c.Change();
+      }
     }
   }
   
@@ -121,6 +146,5 @@ public class GUI extends JFrame implements ActionListener{
       }
     }
         
-    }
   }
 }
--- Main.java
+++ Main.java
@@ -1,31 +1,5 @@
 class Main {
   public static void main(String[] args) {
     GUI g = new GUI();
-    Cola c = new Cola(g);
-      for(int n=0; n<5; n++){
-        c.Insert2(g.date[n][0],g.date[n][1],n);//id, rafaga,posicion
-      }
-    c.Calcula();
-    c.DeployCola();
-    System.out.println();
-    c.Change();
-
-    int l=1;
-    int l2=0;
-    //System.out.println("id cliente" +"  "+"transacciones");
-    for(int i=0; true; i++){
-      int num = (int)(Math.random()*4+2); //2-4
-      for(int n=1; n<num; n++){
-         c.Insert(l,(n+l2));
-         l++;
-       }
-        if(l2==0){
-          l2++;
-        }
-        c.Calcula();
-        c.DeployCola();
-        System.out.println();
-        c.Change();
-      }
   }
 }
